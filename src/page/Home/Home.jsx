631afe02f63993b428de9e5ce0922acedd9fe277
Mahim <mdmahim924214@gmail.com>
2025-03-15 06:54:41 +0600

    first commit in dev

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -12,39 +12,7 @@ import {
 } from "@heroui/react";
 
 const Home = () => {
-  return (
-    <div>
-      <Card className="max-w-[400px]">
-        <CardHeader className="flex gap-3">
-          <Image
-            alt="heroui logo"
-            height={40}
-            radius="sm"
-            src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-            width={40}
-          />
-          <div className="flex flex-col">
-            <p className="text-md">HeroUI</p>
-            <p className="text-small text-default-500">heroui.com</p>
-          </div>
-        </CardHeader>
-        <Divider />
-        <CardBody>
-          <p>Make beautiful websites regardless of your design experience.</p>
-        </CardBody>
-        <Divider />
-        <CardFooter>
-          <Link
-            isExternal
-            showAnchorIcon
-            href="https://github.com/heroui-inc/heroui"
-          >
-            Visit source code on GitHub.
-          </Link>
-        </CardFooter>
-      </Card>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
