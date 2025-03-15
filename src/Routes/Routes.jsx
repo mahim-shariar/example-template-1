45f8bcee1b69ce11c46020deb375b1c35c3d98f8
Mahim <mdmahim924214@gmail.com>
2025-03-15 06:53:43 +0600

    first commit in dev

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,9 +2,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import { HeroUIProvider } from "@heroui/react";
-// Lazy loading components
 const Main = lazy(() => import("../Layout/Main"));
 const Home = lazy(() => import("../page/Home/Home"));
+
 export const router = createBrowserRouter([
   {
     path: "/",
